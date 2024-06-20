b7a962362f037da9ee18758aa786a1e6ad5ca2a1
Misty De Méo <misty@axo.dev>
2024-06-20 10:02:42 -0700

    fix: version number display

--- axolotlsay-js/index.js
+++ axolotlsay-js/index.js
@@ -1,8 +1,12 @@
 #! /usr/bin/env node
 
-const yargs = require('yargs/yargs');
-const { hideBin } = require('yargs/helpers');
-const argv = yargs(hideBin(process.argv)).argv;
+const yargs = require("yargs/yargs");
+const { hideBin } = require("yargs/helpers");
+const argv = yargs(hideBin(process.argv)).version(
+  "--version",
+  "Show version number",
+  "0.6.0",
+).argv;
 
 const msg = argv.msg;
 console.log(msg);
