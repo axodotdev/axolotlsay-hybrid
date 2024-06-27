543b54a1777b1810aedfc0f43253327ff950f5fe
Misty De Méo <misty@axo.dev>
2024-06-27 11:45:14 -0700

    fix: hardcode the version flag
    When built with bun, the version can be displayed correctly if the
    binary is run inside the source directory and returns `unknown` otherwise.

--- axolotlsay-js/index.js
+++ axolotlsay-js/index.js
@@ -2,7 +2,11 @@
 
 const yargs = require("yargs");
 const { hideBin } = require("yargs/helpers");
-const argv = yargs(hideBin(process.argv)).argv;
+const argv = yargs(hideBin(process.argv)).version(
+  "--version",
+  "Show version number",
+  "0.10.0",
+).argv;
 
 const msg = argv.msg;
 console.log(msg);
